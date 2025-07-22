802cf0528a1043695d8061b1703b5128672ba5ff
Addison Torres Grant <addison@exnumberless.com>
2025-07-21 18:58:31 -0700

    Rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // IMPORTANT: Change this to the name of your GitHub repository.
-  base: '/virtual-soundboard/',
+  base: '/VibeSoundBoard/',
   plugins: [react()],
 })
